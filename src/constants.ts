e5a8f9f09471ce0246dd48999bbc00227eebe71f
ObservatorioCulPatBoy <observatorioculturapatrimonioboyaca@outlook.es>
2026-07-15 12:01:35 -0500

    Delete src directory

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const FLAG_MAP: Record<string, string> = {
-  'Tunja': '/img/Tunja.png',
-  'Duitama': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Flag_of_Duitama.svg/800px-Flag_of_Duitama.svg.png',
-  'Sogamoso': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Flag_of_Sogamoso.svg/800px-Flag_of_Sogamoso.svg.png',
-  'Paipa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Flag_of_Paipa.svg/800px-Flag_of_Paipa.svg.png',
-};
